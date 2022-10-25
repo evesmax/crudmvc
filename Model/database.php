c61e4ebd05c94dbcc1bccf138dac07d15eb6737e
DyahirVG <dx5yar2@gmail.com>
2022-10-25 08:54:34 -0500

    Mi primer commit

--- Model/database.php
+++ Model/database.php
@@ -10,7 +10,7 @@ class Database
 
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=mvc_php;charset=utf8', 'root', 'root');
+        $pdo = new PDO('mysql:host=localhost;dbname=mvc_php;charset=utf8', 'root', '');
         //Filtrando posibles errores de conexión.
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
